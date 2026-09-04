6ae7407132cc4146b106af5cbba6018279d46633
Moundirbechikh <moundirbechikh03@gmail.com>
2026-09-05 00:13:30 +0100

    first commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,7 @@
 const Order = require('../models/Order');
 const Cart = require('../models/Cart');
 const Product = require('../models/Product');
-const transporter = require('../config/email');
+const transporter = require('../config/Email');
 const {
   orderConfirmationEmailTemplate,
   adminOrderNotificationTemplate,
